b3d972555d3838b78393b90cb508384ed73f4809
Pravallika <pravallikapaluri146@gmail.com>
2022-05-18 10:18:17 +0530

    successfully commited

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,6 @@ const express = require("express");
 const controllers = require("../controllers");
 const router = express.Router();
 
-router.route("/").get(controllers.getAllsample);
+router.route("/").put(controllers.getAllsample);
 router.route("/:id").put(controllers.updateTodo);
 module.exports = router;
